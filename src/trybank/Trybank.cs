45a9e04323d61873f5d047da0ddea2bbecbf819b
wilmaximodev <wilson-max@hotmail.com>
2024-02-28 09:15:10 -0300

    Requisito 7

--- src/trybank/Trybank.cs
+++ src/trybank/Trybank.cs
@@ -97,7 +97,21 @@ public class TrybankLib
     // 7. Construa a funcionalidade de transferir dinheiro entre contas
     public void Transfer(int destinationNumber, int destinationAgency, int value)
     {
-        throw new NotImplementedException();
+        if (!Logged) throw new AccessViolationException("Usuário não está logado");
+
+        for (int i = 0; i < registeredAccounts; i++)
+        {
+            if (Bank[i, 0] == destinationNumber && Bank[i, 1] == destinationAgency)
+            {
+                if (Bank[loggedUser, 3] < value) throw new InvalidOperationException("Saldo insuficiente");
+
+                Bank[loggedUser, 3] -= value;
+                Bank[i, 3] += value;
+                return;
+            }
+        }
+
+        throw new ArgumentException("Agência + Conta não encontrada");
     }
 
    
