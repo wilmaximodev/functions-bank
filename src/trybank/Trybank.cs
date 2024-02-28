9f9ec1528e932b399c5e1014f536634d7785ddfb
wilmaximodev <wilson-max@hotmail.com>
2024-02-28 09:13:21 -0300

    Requisito 6

--- src/trybank/Trybank.cs
+++ src/trybank/Trybank.cs
@@ -87,7 +87,11 @@ public class TrybankLib
     // 6. Construa a funcionalidade de sacar dinheiro
     public void Withdraw(int value)
     {
-        throw new NotImplementedException();
+        if (!Logged) throw new AccessViolationException("Usuário não está logado");
+
+        if (Bank[loggedUser, 3] < value) throw new InvalidOperationException("Saldo insuficiente");
+
+        Bank[loggedUser, 3] -= value;
     }
 
     // 7. Construa a funcionalidade de transferir dinheiro entre contas
